48af099efa09cf8cff077a4de7e6aa908d4a02a6
maritere <mtnietotcpsi@gmail.com>
2018-02-12 12:49:04 +0100

    3 validators

--- validatorN/validator.sh
+++ validatorN/validator.sh
@@ -1,4 +1,3 @@
-#
 sawadm keygen 
 sawtooth keygen my_key 
 sawtooth-validator -vv --endpoint tcp://validator:8800 --bind component:tcp://eth0:4004 --bind network:tcp://eth0:8800 -p tcp://172.16.239.14:8800
